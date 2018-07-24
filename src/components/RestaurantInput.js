8885ec7611fb3ab9c9ebbb6df0148ceb23c7134a
maxwell <github email address>
2018-07-24 09:43:19 -0400

    updated solution

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { addRestaurant } from '../actions/restaurants';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 
 export class RestaurantInput extends Component {
 
@@ -34,15 +33,15 @@ export class RestaurantInput extends Component {
     return(
       <form onSubmit={(event) => this.handleOnSubmit(event)}>
         <p>
-          <input 
-            type="text" 
-            onChange={(event) => this.handleOnNameChange(event)} 
+          <input
+            type="text"
+            onChange={(event) => this.handleOnNameChange(event)}
             placeholder="restaurant name" />
         </p>
         <p>
-          <input 
-            type="text" 
-            onChange={(event) => this.handleOnLocationChange(event)} 
+          <input
+            type="text"
+            onChange={(event) => this.handleOnLocationChange(event)}
             placeholder="location" />
         </p>
         <input type="submit" />
@@ -51,10 +50,4 @@ export class RestaurantInput extends Component {
   }
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({
-    addRestaurant,
-  }, dispatch);
-};
-
-export const ConnectedRestaurantInput = connect(null, mapDispatchToProps)(RestaurantInput);
+export const ConnectedRestaurantInput = connect(null, { addRestaurant })(RestaurantInput);
