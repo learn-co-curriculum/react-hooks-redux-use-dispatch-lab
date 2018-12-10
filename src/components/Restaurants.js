b3df59b230b7a4e48f56f90b47e17c6f26037562
Melanie MacDonald <melmacd94@gmail.com>
2018-12-10 11:02:24 -0500

    typo
    changed restaurantName to name which is the value in state

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -5,7 +5,7 @@ class Restaurants extends Component {
 
   render() {
 
-    let restaurants = this.props.restaurants.map((restaurant, index) => <li key={index}>{restaurant.restaurantName}</li>);
+    let restaurants = this.props.restaurants.map((restaurant, index) => <li key={index}>{restaurant.name}</li>);
 
     return (
       <div>
