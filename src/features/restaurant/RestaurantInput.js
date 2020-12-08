a18fcbb010fb95d6785b807d37c865f70c7430eb
ihollander <ianholla@gmail.com>
2020-12-08 12:31:27 -0500

    Added solution

--- src/features/restaurant/RestaurantInput.js
+++ src/features/restaurant/RestaurantInput.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import { addRestaurant } from "./restaurantsSlice";
 
 function RestaurantInput() {
   const [name, setName] = useState("");
   const [location, setLocation] = useState("");
+
+  const dispatch = useDispatch();
 
   function handleNameChange(event) {
     setName(event.target.value);
@@ -15,7 +18,7 @@ function RestaurantInput() {
 
   function handleSubmit(event) {
     event.preventDefault();
-    // add missing code
+    dispatch(addRestaurant({ name, location }));
   }
 
   return (
