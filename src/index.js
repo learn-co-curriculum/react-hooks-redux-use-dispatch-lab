206678c1201223f0f65ec0dd0bda1aec5707f99a
ihollander <ianholla@gmail.com>
2020-12-28 07:48:56 -0500

    Fixed index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import manageRestaurants from './reducers/manageRestaurants';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
-const store = createStore(manageRestaurants, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+import restaurantsReducer from "./features/restaurant/restaurantsSlice";
+
+const store = createStore(restaurantsReducer);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App store={store}/>
+    <App store={store} />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
